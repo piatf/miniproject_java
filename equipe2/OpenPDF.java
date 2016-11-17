f83eb68dd85e9fd684c6932258d0854c561a599d
GaetanCoulhon <gaetan.coulhon@insa-cvl.fr>
2016-11-17 09:48:36 +0100

    Version finale de la classe
    il faut juste remplacer ce qu'il y a dans la class bibliothèque par le nom des fonctions présentes dans la classe

--- equipe2/OpenPDF.java
+++ equipe2/OpenPDF.java
@@ -1,84 +1,81 @@
+package bibliotheque;
 
-/*
-	Class to Load/Save PDF from explorer into a variable and Read PDF from Path
-	By GaÃ«tan
-	*/
-	
-
-	import java.awt.Desktop;
+import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
 import static java.nio.file.StandardCopyOption.*;
 
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-	
+/**
+ * Class to Save/Load PDF from explorer into a variable and Read PDF from Path
+ * 
+ * 
+ * @author Gaëtan COULHON
+*/
 public class OpenPDF {
-	
-		public String SearchPDF() throws IOException {
-			String s[]=new String [2];
-			JFileChooser chooser = new JFileChooser(); // Create a JFileChooser
-			
-			FileNameExtensionFilter filter = new FileNameExtensionFilter("PDF Only", "pdf");
-			
-			chooser.setFileFilter(filter);
-			
-			chooser.showOpenDialog(null);
-
-			 // if the file can be opened
-				// To copy a file from source to dest
-				File source = new File(chooser.getSelectedFile().getPath());
-				
-				
-				
-				String dest = "PDF\\";
-				String destt = "PDF";
-				
-				Path path = Paths.get(chooser.getSelectedFile().getPath());
-				Path path2 = Paths.get(dest);
-				Path pathh = Paths.get(destt);
-
-				
-				try {
-					Files.copy(path, path2,REPLACE_EXISTING);
-				
-						
-					
-					
-					
-				} catch (IOException e) {
-					//theDir.mkdirs();
-					Files.createDirectory(pathh);
-
-					Files.copy(path, path2,REPLACE_EXISTING);
-					e.printStackTrace();
-					
-				}
-				String name = chooser.getName();
-				
-				//return chooser.getSelectedFile().getPath();
-				return name;
-			}	
+	/**
+	 * Open explorer to search a PDF file,
+	 * Copy it into the directory PDF inside the project.
+	 * 
+	 * @return the String corresponding to the full name of the PDF.
+	 */
+	public String SearchPDF() throws IOException {
 		
+		JFileChooser chooser = new JFileChooser(); // Create a JFileChooser
 		
-		public void ReadPDF (String PathPDF) {
-			try { 
-				File myFile = new File(PathPDF);
-				if (Desktop.isDesktopSupported()) { // open PDF File
-					
-						Desktop.getDesktop().open(myFile);
-					
-				}
-			}
-			catch (IOException e){
-				//Can't open File
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("PDF Only", "pdf"); // Create Filter to search PDF only
+		
+		chooser.setFileFilter(filter); // set Filter
+		
+		chooser.showOpenDialog(null);
+
+		//File source = new File(chooser.getSelectedFile().getPath()); // get full path of the selected PDF
+		Path source = Paths.get(chooser.getSelectedFile().getPath()); // get full path of the selected PDF
+		
+		Path dest = Paths.get("PDF\\"); // create a Path for the destination where to copy the PDF
+		
+		File f = new File(""); // create an empty file in order to catch the Path of the existing project
+		File file = new File(f.getPath()+"PDF"); // create a new directory called PDF in the current project
+		if (!file.exists()) {
+		    if (file.mkdir()) {
+		        System.out.println("Directory is created!");
+		    } else {
+		        System.out.println("Failed to create directory!");
+		    }
+		}
+		
+		Files.copy(source, dest, REPLACE_EXISTING); // copy the PDF from the source to the destination (PDF directory in the project)
+		
+		String name = chooser.getName(); // Save the name of the selected PDF
+		
+		//return chooser.getSelectedFile().getPath();
+		return name; //return the full name of the PDF
+	}
+
+	/**
+	 * Read a PDF from its Path
+	 * 
+	 * @param PathPDF
+	 * 					The Full Path to the PDF
+	 */
+	public void ReadPDF (String PathPDF) {
+		try { 
+			File myFile = new File(PathPDF);
+			if (Desktop.isDesktopSupported()) { // open PDF File
+					Desktop.getDesktop().open(myFile);
 			}
 		}
+		catch (IOException e){
+			//Can't open File
+		}
 	}
+}
 	
 	//https://docs.oracle.com/javase/8/docs/api/javax/swing/JFileChooser.html
 	//http://docs.oracle.com/javase/tutorial/uiswing/components/filechooser.html
